e4e76934a99ddfd910a2c96ac566ec10926b1c52
Liiift <hello@liiift.studio>
2026-07-05 00:27:41 -0700

    Lift ceiling nudge: refresh OG image + favicon for slightly-less-white background

--- site/src/app/opengraph-image.tsx
+++ site/src/app/opengraph-image.tsx
@@ -18,7 +18,7 @@ export default async function OGImage() {
 		(
 			<div
 				style={{
-					background: '#e0fefd',
+					background: '#bdfdfb',
 					width: '100%',
 					height: '100%',
 					display: 'flex',
